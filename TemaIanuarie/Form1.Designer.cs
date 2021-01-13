2ce408d2e906a5b351066ec3d954172afedc3ba7
PodiWan <tpodina@gmail.com>
2021-01-13 17:02:54 +0200

    init commit

--- TemaIanuarie/Form1.Designer.cs
+++ TemaIanuarie/Form1.Designer.cs
@@ -28,13 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sideMenu = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.flowPanelMaster = new System.Windows.Forms.FlowLayoutPanel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.sideMenu.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowPanelMaster.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sideMenu
+            // 
+            this.sideMenu.BackColor = System.Drawing.Color.Black;
+            this.sideMenu.Controls.Add(this.tableLayoutPanel1);
+            this.sideMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sideMenu.Location = new System.Drawing.Point(0, 0);
+            this.sideMenu.Name = "sideMenu";
+            this.sideMenu.Size = new System.Drawing.Size(102, 450);
+            this.sideMenu.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.flowPanelMaster, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnHome, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnClose, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(102, 450);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackColor = System.Drawing.Color.DarkRed;
+            this.btnHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Location = new System.Drawing.Point(3, 3);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(96, 23);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.Text = "button1";
+            this.btnHome.UseVisualStyleBackColor = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.DarkRed;
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Location = new System.Drawing.Point(3, 424);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(96, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "button2";
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // flowPanelMaster
+            // 
+            this.flowPanelMaster.Controls.Add(this.button3);
+            this.flowPanelMaster.Controls.Add(this.button4);
+            this.flowPanelMaster.Controls.Add(this.button5);
+            this.flowPanelMaster.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowPanelMaster.Location = new System.Drawing.Point(3, 93);
+            this.flowPanelMaster.Name = "flowPanelMaster";
+            this.flowPanelMaster.Size = new System.Drawing.Size(96, 264);
+            this.flowPanelMaster.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(3, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(3, 32);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(3, 61);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sideMenu);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.sideMenu.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowPanelMaster.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel sideMenu;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowPanelMaster;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnClose;
     }
 }
 
--- TemaIanuarie/Form1.cs
+++ TemaIanuarie/Form1.cs
@@ -15,6 +15,23 @@ namespace TemaIanuarie
         public Form1()
         {
             InitializeComponent();
+            this.btnHome.BackColor = ColorTranslator.FromHtml("#fafafa");
+            this.btnClose.BackColor = ColorTranslator.FromHtml("#fafafa");
+
+            StyleButtons(this.flowPanelMaster);
+        }
+
+        public void StyleButtons(Control parent)
+        {
+            foreach (Control ctrl in parent.Controls)
+                if (ctrl is Button)
+                {
+                    Button btn = (Button)ctrl;
+                    btn.BackColor = ColorTranslator.FromHtml("#fafafa");
+                    btn.FlatAppearance.MouseOverBackColor = ColorTranslator.FromHtml("#fcf4f4");
+                    btn.Width = this.flowPanelMaster.Width;
+                    btn.Height = this.flowPanelMaster.Height / this.flowPanelMaster.Controls.Count;
+                }
         }
     }
 }
